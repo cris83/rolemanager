958f52f6b16e4e6be72670912f5c03dd9da659c2
cris <kimyt17@hanmail.net>
2014-08-14 11:07:58 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ int main(void)
 		printf("Hello, World!\n");
 		printf("Hello, Jenkins!\n");
 		printf("Hello, cris!\n");
-		printf("Hello, invoke post build task!\n");
+		printf("Hello, invoke post build task.\n");
 		
 		return 0;
 }
